1aa526c8b0c0f9d65b2c68757bed7153e2833090
iGNiTe <ignarmezh@gmail.com>
2018-05-12 00:51:11 +0300

    Main movement with keyboard and joystick

---
+++ Cubes/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5f; //скорость плеера
+
+    Camera viewCamera; //экземпляр камеры
+    PlayerController controller; //экземпляр контроллера
+    public VirtualJoystick virtualJoystick; //экземпляр скрипта управления для телефона
+
+    void Start () {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+	
+	void Update () {
+        //Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        Vector3 moveInput = new Vector3(virtualJoystick.Horizontal(),0,virtualJoystick.Vertical()); //принимаем параметры для определения направления движения
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed; //преобразование в конечный вектор движения плеера
+        controller.Move(moveVelocity);
+
+        //Определение направления поворота плеера в нужную сторону
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up,Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray,out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            controller.LookAt(point);
+        }
+	}
+}
---
+++ Cubes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Vector3 velocity;
+
+    Rigidbody myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity) //принимаем вектор в направлении которого будет совершено движение
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 _point) //поворот плеера в нужную сторону
+    {
+        Vector3 correctPoint = new Vector3(_point.x,transform.position.y,_point.z); //корректировка поворота, чтобы плеер не наклонялся
+        transform.LookAt(correctPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
