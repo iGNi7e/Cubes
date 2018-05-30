37fc92a64ab115cc444380f870a63c2e26865839
iGNiTe <ignarmezh@gmail.com>
2018-05-30 23:14:06 +0300

    Flood fill

--- Cubes/Assets/Scripts/GameElementUI.cs
+++ Cubes/Assets/Scripts/GameElementUI.cs
@@ -8,10 +8,16 @@ public class GameElementUI : MonoBehaviour {
     
     public Text enemyCountText;
     public Slider enemyCountSlider;
+
     public Text timeBetweenSpawnText;
     public Slider timeBetweenSpawnSlider;
 
+    public Text obstaclePercentText;
+    public Slider obstaclePercentSlider;
+
     public Spawner spawner;
+
+    public MapGenerator mapGenerator;
 
     private void Update()
     {
@@ -20,6 +26,10 @@ public class GameElementUI : MonoBehaviour {
 
         spawner.waves[0].timeBetweenSpawn = timeBetweenSpawnSlider.value;
         timeBetweenSpawnText.text = "Time between spawn enemies: " + spawner.waves[0].timeBetweenSpawn + " sec";
+
+        mapGenerator.obstaclePercent = obstaclePercentSlider.value;
+        obstaclePercentText.text = "Obstacle Percent: " + mapGenerator.obstaclePercent;
+        mapGenerator.GenerateMap();
     }
 
     public void SpawnerEnable()
--- Cubes/Assets/Scripts/MapGenerator.cs
+++ Cubes/Assets/Scripts/MapGenerator.cs
@@ -11,10 +11,14 @@ public class MapGenerator : MonoBehaviour {
     [Range(0,1)]
     public float outlinePercent; //размер расстояния между тайлами
 
+    [Range(0,1)]
+    public float obstaclePercent;
+
     List<Coord> allTileCoords; //координаты всех тайлов
     Queue<Coord> shuffledTileCoords; //перетасованные координаты
 
     public int seed = 10;
+    Coord mapCenter;
 
     private void Start()
     {
@@ -32,6 +36,7 @@ public class MapGenerator : MonoBehaviour {
             }
         }
         shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(),seed)); //перетасованная очередь
+        mapCenter = new Coord((int)mapSize.x / 2,(int)mapSize.y / 2);
 
         string holderName = "Generated Map";
         if (transform.Find(holderName))
@@ -53,14 +58,67 @@ public class MapGenerator : MonoBehaviour {
             }
         }
 
-        int obstacleCount = 10;
+        bool[,] obstacleMap = new bool[(int)mapSize.x,(int)mapSize.y];
+
+        int obstacleCount = (int)(mapSize.x * mapSize.y * obstaclePercent);
+        int currentObstacleCount = 0;
         for (int i = 0; i < obstacleCount; i++) //создание стен
         {
             Coord randomCoord = GetRandomCoord();
-            Vector3 obstaclePosition = CoordToPosition(randomCoord.x,randomCoord.y);
-            Transform newObstacle = Instantiate(obstaclePrefab,obstaclePosition + Vector3.up * 0.5f,Quaternion.identity) as Transform;
-            newObstacle.parent = mapHolder;
+            obstacleMap[randomCoord.x,randomCoord.y] = true;
+            currentObstacleCount++;
+            if (randomCoord.x != mapCenter.x && randomCoord.y != mapCenter.y && MapIsFullAccessible(obstacleMap,currentObstacleCount))
+            {
+                Vector3 obstaclePosition = CoordToPosition(randomCoord.x,randomCoord.y);
+
+                Transform newObstacle = Instantiate(obstaclePrefab,obstaclePosition + Vector3.up * 0.5f,Quaternion.identity) as Transform;
+                newObstacle.parent = mapHolder;
+            }
+            else
+            {
+                obstacleMap[randomCoord.x,randomCoord.y] = false;
+                currentObstacleCount--;
+            }
         }
+    }
+
+    bool MapIsFullAccessible(bool[,] obstacleMap, int currentObstacleCount) //определение возможности поставки препятствия на карте, алгоритм заливки Flood fild
+    {
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0),obstacleMap.GetLength(1)];
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(mapCenter);
+        mapFlags[mapCenter.x,mapCenter.y] = true;
+
+        int accessibleTileCount = 1;
+
+        while(queue.Count > 0)
+        {
+            Coord tile = queue.Dequeue();
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    int neighbourX = tile.x + x;
+                    int neighbourY = tile.y + y;
+                    if (x == 0 || y == 0)
+                    {
+                        if(neighbourX >=0 && neighbourX < obstacleMap.GetLength(0) && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1))
+                        {
+                            if(!mapFlags[neighbourX,neighbourY] && !obstacleMap[neighbourX,neighbourY])
+                            {
+                                mapFlags[neighbourX,neighbourY] = true;
+                                queue.Enqueue(new Coord(neighbourX,neighbourY));
+                                accessibleTileCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int targetAccessibleTielCount = (int)(mapSize.x * mapSize.y - currentObstacleCount);
+        return targetAccessibleTielCount == accessibleTileCount;
     }
 
     Vector3 CoordToPosition(int x, int y)
